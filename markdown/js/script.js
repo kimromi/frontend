d0dbc4c0435106f3ff3b890ba61ab566032b7aee
hiromikimura <hiromi19860101@gmail.com>
2014-10-06 16:48:41 +0900

    markdown editor

---
+++ markdown/js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+    marked.setOptions({
+        langPrefix: ''
+    });
+
+    $('#edit').keyup(function() {
+        var md = sanitize($(this).val());
+        var html = marked(md);
+        $('#preview').html(html);
+        $('#preview pre code').each(function(i, e) {
+            $(e).text(unsanitize($(e).text()));
+            hljs.highlightBlock(e, e.className);
+        });
+    });
+
+});
+
+function sanitize(html) {
+    return $('<div />').text(html).html();
+}
+function unsanitize(html) {
+    return $('<div />').html(html).text();
+}
